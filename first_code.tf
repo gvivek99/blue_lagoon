1f86b83ececb220e38b214b8b9a2fc69655ae8a4
gvivek99 <164705809+gvivek99@users.noreply.github.com>
2024-09-24 06:48:43 -0700

    Add files via upload

---
+++ first_code.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+	profile = "default"
+	region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+	bucket = "my-own-bucket-232312-2019"
+}
+
+resource "aws_s3_bucket_acl" "my_bucket" {
+	bucket = "my-own-bucket-232312-2019"
+        acl = "private"
+}
+
