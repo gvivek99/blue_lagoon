8015f4eb45001ad27e7e1f5c822ddb1323fde184
gvivek99 <164705809+gvivek99@users.noreply.github.com>
2024-09-24 11:02:44 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_vpc" "default"{
 	default = "true"
 }
 
-resource "aws_instacnce" "blog"{
+resource "aws_instance" "blog"{
 	ami = "data.aws_ami.app_ami.id"
 	instance_type = var.instance_type
 	
